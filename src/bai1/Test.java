a7e5d82048c07f420fcab93bdd415b38855f9022
unknown <huyhoang2k3hb@gmail.com>
2021-11-26 14:51:45 +0700

    fix code

--- src/bai1/Test.java
+++ src/bai1/Test.java
@@ -3,5 +3,6 @@ package bai1;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Tạo lại các class nhé ");
+        System.out.println("Welcome to Plusplus AcadeMy");
     }
 }
